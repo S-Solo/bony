4ca72e0a23738ebab562936523a88a044e979429
Sokrat Poghosyan <sokrat_poghosyan@epam.com>
2021-09-30 01:50:58 +0400

    Updates

---
+++ static/js/3.f8be4498.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpbony=this.webpackJsonpbony||[]).push([[3],{29:function(n,i,t){"use strict";t.r(i);t(0);var s=t(1);i.default=function(){return Object(s.jsx)("div",{children:"initial"})}}}]);
+//# sourceMappingURL=3.f8be4498.chunk.js.map
